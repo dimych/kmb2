3f269048cb104627be155ca2b3380426e1bb7a94
Dmitry Bukin <dmitrij.bukin1988@gmail.com>
2017-12-19 19:42:23 +0300

    apples_project_js

---
+++ FuitsBaskets/Dmitry Bukin_Basket_Clear/list.js
@@ -0,0 +1,22 @@
+var apple1 = new Apple('orange app', true);
+var apple2 = new Apple('red app', false);
+var apple3 = new Apple('green app', true);
+var apple4 = new Apple('yellow app', false);
+var apple5 = new Apple('white app', true);
+
+
+var pear1 = new Pear('Edd', 'Anjou');
+var pear2 = new Pear('Hank', 'Comice');
+var pear3 = new Pear('George', 'Bosc');
+var pear4 = new Pear('John', 'Bartlett');
+var pear5 = new Pear('Mahatma', 'Asian');
+
+
+var orange1 = new Orange('Mamba', 'Nigeria');
+var orange2 = new Orange('Saddam', 'Irak');
+var orange3 = new Orange('Kim', 'North Korea');
+var orange4 = new Orange('Raul', 'Marokko');
+var orange5 = new Orange('Achmed', 'Dagestan');
+
+var basket = new Basket();
+
