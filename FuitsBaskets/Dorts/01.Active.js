c032ffb2ca85e2cbe47c3ac05245c88abd33e573
aniolka <gothic_aniol@mail.ru>
2017-12-19 21:47:16 +0300

    Add fruit basket

--- FuitsBaskets/Dorts/01.Active.js
+++ FuitsBaskets/Dorts/01.Active.js
@@ -3,11 +3,9 @@ basket.addProduct(apple2);
 basket.addProduct(orange1);
 basket.addProduct(orange2);
 
-var allApplesFromBasket = basket.getAllApples();
-for (var i=0; i<allApplesFromBasket.length; i++) {
-    console.log(allApplesFromBasket[i].name)
-}
+basket.getAllApples();
+basket.getAllOrange();
 
 basket.clear();
-var allApplesFromBasket = basket.getAllApples();
-console.log(allApplesFromBasket.length === 0);
+// var allApplesFromBasket = basket.getAllApples();
+// console.log(allApplesFromBasket.length === 0);
--- FuitsBaskets/Dorts/01.Constructor.js
+++ FuitsBaskets/Dorts/01.Constructor.js
@@ -14,31 +14,48 @@ function Orange(name, country) {
 };
 
 function Basket() {
-    this.allAppleObject = [];
-    this.allPearObject = [];
-    this.allOrangeObject = [];
+    this._allAppleObject = [];
+    this._allPearObject = [];
+    this._allOrangeObject = [];
 
     this.addProduct = function (fruit) {
         if (fruit.constructor.name === 'Apple') {
-            this.allAppleObject.push(fruit);
+            this._allAppleObject.push(fruit);
+            console.log('apple');
         } else if (fruit.constructor.name === 'Pear') {
-            this.allPearObject.push(fruit); 
+            this._allPearObject.push(fruit);
+            console.log('per');
         } else if (fruit.constructor.name === 'Orange') {
-            this.allOrangeObject.push(fruit); }       
+            this._allOrangeObject.push(fruit);
+            console.log('orange');
+        }
     }
 
     this.getAllApples = function () {
-
+        var apples = [];
+        for (var i = 0; i < this._allAppleObject.length; i++) {
+            apples.push(this._allAppleObject[i]);
+            console.log(apples[i].name)
+        }
+        return apples;
     }
     this.getAllPears = function () {
 
     }
     this.getAllOrange = function () {
-
+        var oranges = [];
+        for (var i = 0; i < this._allOrangeObject.length; i++) {
+            oranges.push(this._allOrangeObject[i]);
+            console.log(oranges[i].name)
+        }
+        return oranges;
     }
-    this.clear = function() {
-    this.apples = [];
-}
+    this.clear = function () {
+        this._allAppleObject = [];
+        this._allOrangeObject = [];
+        // console.log(this._allAppleObject);
+        // console.log(this._allOrangeObject);
+    }
 };
 
 
