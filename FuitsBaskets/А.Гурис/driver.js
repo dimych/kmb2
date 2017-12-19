64ea456ed8061a8aa2dd9bc66adfe7b13de78695
Sasha <sasha.gurys@gmail.com>
2017-12-19 19:59:30 +0300

    deleted russian folder

--- FuitsBaskets/А.Гурис/driver.js
+++
@@ -1,9 +0,0 @@
-function Driver(fn, ln, car) {
-    this.firstName = fn;
-    this.lastName = ln;
-    this.sound = null;
-    this.car = car;
-    this.startCar = function () {
-        this.sound = this.car._engine.start();
-    }
-}
