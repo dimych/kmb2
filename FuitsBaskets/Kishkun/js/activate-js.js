2e02445d3215713643a83c1e2dac089cbd78adda
Kishkun <durata55@gmail.ru>
2017-12-27 00:08:41 +0300

    add kishkun dal-lc.js

---
+++ FuitsBaskets/Kishkun/js/activate-js.js
@@ -0,0 +1,20 @@
+var basketApp = new BasketApp();
+var basket = new Basket();
+basket.addProduct(apple1);
+basket.addProduct(apple2);
+basket.addProduct(pear1);
+basket.addProduct(pear2);
+basket.addProduct(orange1);
+basket.addProduct(orange2);
+
+
+basket.getAllApples();
+basket.getAllPear();
+basket.getAllOrange();
+
+basket.clear();
+
+
+
+
+
---
+++ FuitsBaskets/Kishkun/js/construktor.js
@@ -0,0 +1,66 @@
+function Apple(name, isWinter) {
+    this.name = name;
+    this.isWinter = isWinter;
+}
+
+function Pear(name, type) {
+    this.name = name;
+    this.type = type;
+}
+
+function Orange(name, country) {
+    this.name = name;
+    this.country = country;
+}
+
+function Basket() {
+    this._apples = [];
+    this._pears = [];
+    this._oranges = [];
+
+    this.addProduct = function (fruits) {
+        if (fruits.constructor == Apple) this._apples.push(fruits);
+        if (fruits.constructor == Pear) this._pears.push(fruits);
+        if (fruits.constructor == Orange) this._oranges.push(fruits);
+    }
+
+    this.getAllApples = function (typeOfProduct) {
+        if (typeOfProduct === Apple)
+            for (var i = 0; i < this_apples.length; i++) {
+                console.log(this._apples[i]);
+            }
+    }
+
+    this.getAllPear = function (typeOfProduct) {
+        if (typeOfProduct === Pear)
+            for (var i = 0; i < this._pears.length; i++) {
+                console.log(this._pears[i]);
+            }
+
+    this.getAllOrange = function (typeOfProduct) {
+        if (typeOfProduct === Orange)
+            for (var i = 0; i < this._oranges.length; i++) {
+                console.log(this._oranges[i]);
+            }
+        }
+
+        this.clear = function () {
+            this._apples = [];
+            this._pears = [];
+            this._oranges = [];
+        }
+    }
+}
+    
+
+    function BasketApp() {
+        $('div').append('<select><option>Apple</option><option>Orange</option><option>Pear</option></select>');
+        $('div').append('<button class = createFruits>Create Fruits</button>');
+        $('div').append('<button class = saveBtn>Save Fruits</button>');
+        $('div').append('<button class = loadBtn>Load Fruits</button>');
+        $('.createFruits').on('click', function () {
+            console.log('click me');
+        });
+    }
+
+
