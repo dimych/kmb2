4a706d6b6f96ce747fd804684affa97e775194b4
aniolka <gothic_aniol@mail.ru>
2018-01-24 21:39:09 +0300

    css Slider 2

--- Slider/UI2Slider.js
+++ Slider/UI2Slider.js
@@ -6,5 +6,17 @@ SliderViewPretty.prototype = new SliderView();
 SliderViewPretty.prototype.constructor = SliderViewPretty;
 SliderViewPretty.prototype._render = function () {
     SliderView.prototype._render.apply(this);
-    this.el.innerHTML += `<hr><hr>`
+    this.el.innerHTML =  `
+    <hr>
+    <div class='block'>
+    <button id='prev1' class='prev'>PREV</button>
+    <img class='image' />
+    <button id='next1' class='next'>NEXT</button>
+    </div>
+    <br>
+    <div class='block'>
+    <input type = "checkbox" id='isCycled'>CYCLED</input>
+    </div>
+    <hr>        
+    `
 };
